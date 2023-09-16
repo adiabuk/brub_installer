b649929b4a3612106cef7de3caa36545cb7ce2dc
Amro Diab <adiab@hotmail.co.uk>
2023-09-16 20:04:15 +0100

    improve script and add kernel params

--- make_boot.sh
+++ make_boot.sh
@@ -1,19 +1,22 @@
-mkfs.vfat -F32 -s2 /dev/sdg1
-mount /dev/sdg1 /media/USB
-cd ~/grub
-cd ./EFI64/grub-core
-grub-install -d $PWD --force --removable --no-floppy --target=x86_64-efi --boot-directory=/media/USB/boot --efi-directory=/media/USB
-cd ../../EFI32/grub-core
-grub-install -d $PWD --force --removable --no-floppy --target=i386-efi --boot-directory=/media/USB/boot --efi-directory=/media/USB
-cd ../../BIOS/grub-core
-grub-install -d $PWD --force --no-floppy --target=i386-pc --boot-directory=/media/USB/boot /dev/sdg
-cd /media/USB/boot/grub
-cp ~/media/USB-copy/boot/grub/grub.cfg .
-cd /media/USB
-cp /USB-copy/initrd* .
-cp /USB-copy/vmlinuz* .
-#cp /USB-copy/System.map* .
+#!/usr/bin/env bash
+###
+### Create bootable grub USB Pen or generic disk
+###
+echo "Enter device (eg sde):"
+read device
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+mkfs.vfat -F32 -s2 /dev/${device}
+umount /media/USB || true
+mkdir -p /media/USB
+mount /dev/${device}1 /media/USB
+
+grub-install -d ${SCRIPT_DIR}/EFI64/grub-core --force --removable --no-floppy --target=x86_64-efi --boot-directory=/media/USB/boot --efi-directory=/media/USB
+grub-install -d ${SCRIPT_DIR}/EFI32/grub-core --force --removable --no-floppy --target=i386-efi --boot-directory=/media/USB/boot --efi-directory=/media/USB
+grub-install -d ${SCRIPT_DIR}BIOS/grub-core --force --no-floppy --target=i386-pc --boot-directory=/media/USB/boot /dev/${device}
+cp ${SCRIPT_DIR}/grub.cfg /media/USB/boot/grub/
+cp ${SCRIPT_DIR}/initrd* /media/USB/
+cp ${SCRIPT_DIR}/vmlinuz* /media/USB/
 
 
 
